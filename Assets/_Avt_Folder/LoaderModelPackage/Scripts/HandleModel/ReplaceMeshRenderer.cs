6c4ed47097db96e5c45c9f3e4539578666a7cb93
cuongnv1 <cuongnv1@mirabo-global.com>
2024-12-17 10:59:28 +0700

    feat: add outline

---
+++ Assets/_Avt_Folder/LoaderModelPackage/Scripts/HandleModel/ReplaceMeshRenderer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ReplaceMeshRenderer : MonoBehaviour
+{
+    public MeshRenderer newMeshRenderer;
+
+    void Start()
+    {
+        GameObject targetObject = gameObject;
+
+        if (newMeshRenderer != null)
+        {
+            MeshRenderer oldMeshRenderer = targetObject.GetComponent<MeshRenderer>();
+            if (oldMeshRenderer != null)
+            {
+                Destroy(oldMeshRenderer);
+            }
+
+            MeshRenderer addedMeshRenderer = targetObject.AddComponent<MeshRenderer>();
+
+            addedMeshRenderer.materials = newMeshRenderer.materials;
+            addedMeshRenderer.enabled = newMeshRenderer.enabled;
+            addedMeshRenderer.shadowCastingMode = newMeshRenderer.shadowCastingMode;
+            addedMeshRenderer.receiveShadows = newMeshRenderer.receiveShadows;
+            addedMeshRenderer.lightProbeUsage = newMeshRenderer.lightProbeUsage;
+            addedMeshRenderer.reflectionProbeUsage = newMeshRenderer.reflectionProbeUsage;
+        }
+        else
+        {
+            Debug.LogError("New MeshRenderer is not assigned!");
+        }
+    }
+}
+
