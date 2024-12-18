551495d5385bde374c0b9852209e2a698568aae5
cuongnv1 <cuongnv1@mirabo-global.com>
2024-12-18 09:04:41 +0700

    feat: update

--- Assets/_Avt_Folder/LoaderModelPackage/Scripts/HandleModel/ModelController.cs
+++ Assets/_Avt_Folder/LoaderModelPackage/Scripts/HandleModel/ModelController.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class ModelController : MonoBehaviour
 {
-    private MeshRenderer[] meshRenderers;
     [SerializeField] private GameObject partHandlerPrefab;
     private void Start()
     {
-        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        /*var meshRenderers = GetComponentsInChildren<MeshRenderer>();
 
         foreach (var item in meshRenderers)
         {
             string nameItem = item.name;
             Transform parentItem = item.transform.parent;
             GameObject partGrabObj = Instantiate(partHandlerPrefab, Vector3.zero, Quaternion.identity);
-            partGrabObj.name = "Grab_" + nameItem;
             partGrabObj.transform.SetParent(parentItem);
+            partGrabObj.name = "Grab_" + nameItem;
+            partGrabObj.transform.localPosition = Vector3.zero;
+            partGrabObj.transform.localScale = Vector3.one;
             var partHandler = partGrabObj.GetComponent<PartHandlerCtrl>();
             partHandler.SetData(item);
-        }
+        }*/
     }
 }
--- Assets/_Avt_Folder/LoaderModelPackage/Scripts/HandleModel/PartHandlerCtrl.cs
+++ Assets/_Avt_Folder/LoaderModelPackage/Scripts/HandleModel/PartHandlerCtrl.cs
@@ -1,16 +1,46 @@
 using Oculus.Interaction;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PartHandlerCtrl : MonoBehaviour
 {
-    [SerializeField] Transform rootTrans;
+    private PointableUnityEventWrapper _event;
+    private MeshRenderer _meshRenderer;
+    private void Start()
+    {
+        _event= GetComponent<PointableUnityEventWrapper>();
+        _meshRenderer = GetComponentInChildren<MeshRenderer>();
+
+        _event.WhenSelect.AddListener(x => WhenSelectObject());
+        _event.WhenUnselect.AddListener(x => WhenUnSelectObject());
+    }
+
+
+    /*[SerializeField] Transform rootTrans;
     [SerializeField] MaterialPropertyBlockEditor materialPropertyBlockEditor;
 
     private GameObject itemGrab;
 
     public void SetData(MeshRenderer meshRenderer)
+    {
+        ClearMeshRenderObj();
+        string nameMesh= meshRenderer.gameObject.name;
+        Vector3 posMesh = meshRenderer.transform.localPosition;
+        Quaternion rotMesh = meshRenderer.transform.localRotation;
+        Vector3 scaleMesh = meshRenderer.transform.localScale;
+
+        GameObject meshObj = Instantiate(meshRenderer.gameObject);
+
+        materialPropertyBlockEditor.Renderers.Add(meshRenderer);
+        meshObj.transform.SetParent(rootTrans);
+        meshObj.name = nameMesh;
+        meshObj.transform.localPosition = posMesh;
+        meshObj.transform.localRotation = rotMesh;
+        meshObj.transform.localScale = scaleMesh;
+
+        itemGrab = meshObj.gameObject;
+    }
+
+    private void ClearMeshRenderObj()
     {
         if (rootTrans.childCount > 0)
         {
@@ -20,19 +50,29 @@ public class PartHandlerCtrl : MonoBehaviour
             }
         }
         materialPropertyBlockEditor.Renderers.Clear();
-
-        materialPropertyBlockEditor.Renderers.Add(meshRenderer);
-        meshRenderer.transform.SetParent(rootTrans);
-
-        itemGrab = meshRenderer.gameObject;
-    }
+    }*/
 
     public void WhenSelectObject()
     {
         Debug.LogWarning("WhenSelectObject");
+        Outline outline = _meshRenderer.gameObject.GetComponent<Outline>();
+        if (outline == null)
+        {
+            outline = _meshRenderer.gameObject.AddComponent<Outline>();
+        }
+
+        outline.OutlineColor = Color.red;
+        outline.OutlineWidth = 10f;
+        outline.enabled = true;
+
     }
     public void WhenUnSelectObject()
     {
+        Outline outline = _meshRenderer.gameObject.GetComponent<Outline>();
+        if (outline != null)
+        {
+            outline.enabled = false;
+        }
         Debug.LogWarning("WhenUnSelectObject");
     }
 }
